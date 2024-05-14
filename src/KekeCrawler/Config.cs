3944026be562d9858dfc9a803b8e768edd1dc2d7
keke1210 <berberiskerdi@gmail.com>
2024-05-14 02:02:54 +0200

    removed unussed Config property

--- src/KekeCrawler/Config.cs
+++ src/KekeCrawler/Config.cs
@@ -3,9 +3,8 @@
     public sealed class Config
     {
         public string Url { get; set; }
-        public int? MaxPagesToCrawl { get; set; } = 1000;
-        public string? OutputFileName { get; set; }
         public CookieConfig? Cookie { get; set; }
+        public int? MaxPagesToCrawl { get; set; } = 1000;
         public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(1);
     }
 
