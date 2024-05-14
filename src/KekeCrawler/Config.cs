0a406860ef120a0a795eed568fa1db7a3f1e4080
keke1210 <berberiskerdi@gmail.com>
2024-05-15 01:38:25 +0200

    added summary for PageSelector config param

--- src/KekeCrawler/Config.cs
+++ src/KekeCrawler/Config.cs
@@ -7,6 +7,10 @@
         public int? MaxPagesToCrawl { get; set; } = 1000;
         public TimeSpan HttpRequestTimeout { get; set; } = TimeSpan.FromSeconds(1);
         public TimeSpan OnVisitPageTimeout { get; set; } = TimeSpan.FromSeconds(1);
+
+        /// <summary>
+        /// Obtain the inner HTML of elements that match the specified selector using JS querySelector syntax.
+        /// </summary>
         public string PageSelector { get; set; }
     }
 
